d15c4d029fce05d4d070ed0fed691b48148e3f50
Henri Joss <henri.joss@ise.fraunhofer.de>
2022-05-11 10:48:24 +0200

    added pagination

---
+++ src/components/BasePagination.vue
@@ -0,0 +1,33 @@
+<template>
+  <button @click="$emit('change-page', 1)">First</button>
+  <button v-if="currentPage > 1" @click="$emit('change-page', currentPage - 1)">
+    Prev
+  </button>
+  <button
+    v-if="currentPage < storeGoT.pageAmount"
+    @click="$emit('change-page', currentPage + 1)"
+  >
+    Next
+  </button>
+  <button @click="$emit('change-page', storeGoT.pageAmount)">Last</button>
+</template>
+
+<script>
+import { storeGoT } from "@/store/store.js";
+
+export default {
+  name: "BasePagination",
+  emits: ["change-page"],
+  data() {
+    return {
+      storeGoT,
+    };
+  },
+  props: {
+    currentPage: {
+      type: Number,
+      default: 1,
+    },
+  },
+};
+</script>
