e46b7120e446b43748cb47ab21218ecac63b3520
Henri Joss <henri.joss@ise.fraunhofer.de>
2022-05-11 18:02:56 +0200

    added pagination

--- src/components/BasePagination.vue
+++ src/components/BasePagination.vue
@@ -1,15 +1,29 @@
 <template>
-  <button @click="$emit('change-page', 1)">First</button>
-  <button v-if="currentPage > 1" @click="$emit('change-page', currentPage - 1)">
-    Prev
-  </button>
-  <button
-    v-if="currentPage < storeGoT.pageAmount"
-    @click="$emit('change-page', currentPage + 1)"
-  >
-    Next
-  </button>
-  <button @click="$emit('change-page', storeGoT.pageAmount)">Last</button>
+  <div>
+    <button @click="$emit('change-page', 1)" class="btn btn-pagination">
+      <i class="icon icon-light icon-double-arrow-left"></i>
+    </button>
+    <button
+      :disabled="currentPage == 1"
+      @click="$emit('change-page', currentPage - 1)"
+      class="btn btn-pagination"
+    >
+      <i class="icon icon-light icon-arrow-left"></i>
+    </button>
+    <button
+      :disabled="currentPage == storeGoT.pageAmount"
+      @click="$emit('change-page', currentPage + 1)"
+      class="btn btn-pagination"
+    >
+      <i class="icon icon-light icon-arrow-right"></i>
+    </button>
+    <button
+      @click="$emit('change-page', storeGoT.pageAmount)"
+      class="btn btn-pagination"
+    >
+      <i class="icon icon-light icon-double-arrow-right"></i>
+    </button>
+  </div>
 </template>
 
 <script>
