40a476e9484dfa0d025b54e40d1e3fad626a72df
Henri Joss <henri.joss@ise.fraunhofer.de>
2022-05-13 14:53:32 +0200

    refactor to use composition api

--- src/views/HouseDetail.vue
+++ src/views/HouseDetail.vue
@@ -1,30 +1,48 @@
 <template>
-  {{ house.name }}
+  <div class="container-bg-img bg-map">
+    <router-link :to="{ name: 'home' }">
+      <BaseButton
+        :button-text="'Back'"
+        :button-class="'btn-primary'"
+        :button-icon-left-class="'icon-light icon-arrow-left'"
+      />
+    </router-link>
+
+    <div class="container vh-100">
+      <h1>
+        <hr />
+        {{ house.name }}
+        <hr />
+      </h1>
+      <div class="house-region">
+        <i v-if="house.region" class="icon icon-light icon-explore"></i>
+        <h2>{{ house.region }}</h2>
+      </div>
+      <div v-if="house.coatOfArms" class="house-coat-of-arms">
+        <i class="icon icon-light icon-large icon-coat-of-arms"></i>
+        <h3>{{ house.coatOfArms }}</h3>
+      </div>
+      <h2>{{ house.founded }}</h2>
+    </div>
+  </div>
 </template>
 
 <script>
-import { storeGoT } from "@/store/store.js";
-import { fetchHouseAPI } from "@/api/apiGoT.js";
+import { useRoute } from "vue-router";
+import useHouses from "@/store/useHouses";
+
+import BaseButton from "@/components/BaseButton.vue";
 
 export default {
   name: "HouseDetail",
-  data() {
-    return {
-      storeGoT,
-      house: {},
-    };
+  components: {
+    BaseButton,
   },
-  async created() {
-    // Try to get house from state, if it isnt present fetch it from the api
-    let houseItem = this.getHouseFromState();
-    this.house = houseItem
-      ? houseItem
-      : await fetchHouseAPI(this.$route.params.id);
-  },
-  methods: {
-    getHouseFromState() {
-      return storeGoT.houses.filter((h) => h.id == this.$route.params.id)[0];
-    },
+  setup() {
+    const route = useRoute();
+    const { loadHouse, currentHouse } = useHouses();
+    loadHouse(route.params.id);
+    return { house: currentHouse };
   },
 };
 </script>
