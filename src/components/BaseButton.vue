7f474a5e15796979c64f000bde9c2a894e4c609d
Henri Joss <henri.joss@ise.fraunhofer.de>
2022-05-13 14:42:52 +0200

    added Button

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <button class="btn" :class="buttonClass">
+    <i v-if="buttonIconLeftClass" class="icon" :class="buttonIconLeftClass"></i>
+    <span>{{ buttonText }}</span>
+    <i
+      v-if="buttonIconRightClass"
+      class="icon"
+      :class="buttonIconRightClass"
+    ></i>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "BaseButton",
+  props: {
+    buttonText: {
+      type: String,
+      default: "",
+    },
+    buttonClass: {
+      String,
+      default: "",
+    },
+    buttonIconLeftClass: {
+      type: String,
+      default: "",
+    },
+    buttonIconRightClass: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
