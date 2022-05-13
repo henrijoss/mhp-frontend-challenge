f377e54fc8991b52b41a91be285063962fa304de
Henri Joss <henri.joss@ise.fraunhofer.de>
2022-05-13 14:43:27 +0200

    changed to router link

--- src/components/HouseItem.vue
+++ src/components/HouseItem.vue
@@ -1,18 +1,16 @@
 <template>
-  <div
-    @click="
-      $router.push({
-        name: 'HouseDetail',
-        params: {
-          id: house.id,
-        },
-      })
-    "
+  <router-link
+    :to="{
+      name: 'HouseDetail',
+      params: {
+        id: house.id,
+      },
+    }"
     class="house-link"
   >
     <h2>{{ house.name }}</h2>
     <h3>{{ house.region }}</h3>
-  </div>
+  </router-link>
 </template>
 
 <script>
