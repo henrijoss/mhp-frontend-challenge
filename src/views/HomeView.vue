06a0c8902805f91152bdce60afe0f856804aa8d9
Henri Joss <henri.joss@ise.fraunhofer.de>
2022-05-13 14:53:22 +0200

    refactor to use composition api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,25 @@
 <template>
-  <div class="container-bg-img">
+  <div class="container-bg-img bg-throne">
     <article class="container houses-container">
-      <div v-if="error.state">{{ error.message }}</div>
-      <section v-else>
+      <div v-if="error">{{ error.message }}</div>
+      <section>
         <h1>
           <hr />
           All Houses
           <hr />
         </h1>
-        <ListHouseItem :houses="storeGoT.houses" />
+        <div v-if="loading" class="houses-list">
+          <SkeletonLoader
+            v-for="index in 9"
+            :key="index"
+            :css-class="'house'"
+          />
+        </div>
+        <ListHouseItem v-else :houses="getHouses(currentPage)" />
         <BasePagination
-          :currentPage="storeGoT.currentPage"
-          @change-page="fetchHouses"
+          :current-page="currentPage"
+          :page-amount="pageAmount"
+          @change-page="loadHouses"
           class="houses-pagination"
         />
       </section>
@@ -20,47 +28,32 @@
 </template>
 
 <script>
-// @ is an alias to /src
-import { storeGoT } from "@/store/store.js";
-import { fetchHousesAPI } from "@/api/apiGoT.js";
+import useHouses from "@/store/useHouses";
 
 import ListHouseItem from "@/components/ListHouseItem.vue";
 import BasePagination from "@/components/BasePagination.vue";
+import SkeletonLoader from "@/components/SkeletonLoader.vue";
 
 export default {
   name: "HomeView",
   components: {
     ListHouseItem,
     BasePagination,
+    SkeletonLoader,
   },
-  data() {
+  setup() {
+    const { loading, error, currentPage, pageAmount, loadHouses, getHouses } =
+      useHouses();
+    console.log(loading.value);
+    loadHouses();
     return {
-      storeGoT,
-      loading: true,
-      error: {
-        state: false,
-        message: "",
-      },
+      loading,
+      error,
+      currentPage,
+      pageAmount,
+      loadHouses,
+      getHouses,
     };
-  },
-  async created() {
-    await this.fetchHouses(this.storeGoT.currentPage);
-  },
-  methods: {
-    async fetchHouses(page) {
-      try {
-        this.loading = true;
-        await fetchHousesAPI(page);
-        this.storeGoT.currentPage = page;
-        this.loading = false;
-        console.log(this.storeGoT.houses);
-      } catch (error) {
-        this.loading = false;
-        this.error.state = true;
-        this.error.message = error.message;
-        console.error(error);
-      }
-    },
   },
 };
 </script>
