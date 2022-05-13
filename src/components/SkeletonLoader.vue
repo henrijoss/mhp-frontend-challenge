06d4444d460c5595ec50f8304958b0d22fe5a06d
Henri Joss <henri.joss@ise.fraunhofer.de>
2022-05-13 14:51:57 +0200

    added skeleton laoder

---
+++ src/components/SkeletonLoader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div :style="{ height, width }" :class="cssClass" class="skeleton"></div>
+</template>
+
+<script>
+export default {
+  name: "SkeletonLoader",
+  props: {
+    cssClass: {
+      type: String,
+      default: null,
+    },
+    height: {
+      type: String,
+      default: null,
+    },
+    width: {
+      type: String,
+      default: null,
+    },
+  },
+};
+</script>
