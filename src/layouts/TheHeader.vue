62134b34523fd7090529bd8239d035b7c5adc984
Henri Joss <henri.joss@ise.fraunhofer.de>
2022-05-16 10:53:17 +0200

    added Header

---
+++ src/layouts/TheHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <Transition name="fade-slow" appear>
+      <img src="@/assets/img/logo.png" alt="Logo" />
+    </Transition>
+    <div class="scrolldown" @click="scrollIntoView('featured-houses')">
+      <i class="icon icon-arrow-down"></i>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "TheHeader",
+  methods: {
+    scrollIntoView(id) {
+      document.getElementById(id).scrollIntoView({
+        behavior: "smooth",
+      });
+    },
+  },
+};
+</script>
