15ff4d4afcb6b98a92de7f6cdb2e72e0bcf7ac51
Henri Joss <henri.joss@ise.fraunhofer.de>
2022-05-16 10:52:02 +0200

    added featured houses

---
+++ src/components/FeaturedHouses.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="houses-featured-container" id="featured-houses">
+    <Transition name="fade" appear>
+      <h1 class="banner-heading">Featured Houses</h1>
+    </Transition>
+    <article class="houses-featured">
+      <router-link
+        :to="{
+          name: 'house-detail',
+          params: {
+            id: house.id,
+          },
+        }"
+        v-for="house in houses"
+        :key="house.id"
+        :style="{ backgroundColor: house.color }"
+        class="house-featured"
+      >
+        <img :src="house.imgUrl" alt="house-logo" lazy />
+        <h2>{{ house.shortName }}</h2>
+      </router-link>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FeaturedHouses",
+  props: {
+    houses: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
