fdd64af54c31fb865e5e64518f122755bb1c8e88
Henri Joss <henri.joss@ise.fraunhofer.de>
2022-05-16 10:53:34 +0200

    added featured houses

---
+++ src/static/featuredHouses.js
@@ -0,0 +1,41 @@
+const featuredHouses = [
+  {
+    name: "House Lannister of Casterly Rock",
+    shortName: "Lannister",
+    id: 229,
+    imgUrl: require("@/assets/img/houses/lannister_logo.png"),
+    color: "#7f1b0f",
+  },
+  {
+    name: "House Baratheon of Storm's End",
+    shortName: "Baratheon",
+    id: 17,
+    imgUrl: require("@/assets/img/houses/baratheon_logo.png"),
+    color: "#dec843",
+  },
+
+  {
+    name: "House Stark of Winterfell",
+    shortName: "Stark",
+    id: 362,
+    imgUrl: require("@/assets/img/houses/stark_logo.png"),
+    color: "#fffef0",
+  },
+  {
+    name: "House Tyrell of Highgarden",
+    shortName: "Tyrell",
+    id: 398,
+    imgUrl: require("@/assets/img/houses/tyrell_logo.png"),
+    color: "#5a8529",
+  },
+
+  {
+    name: "House Targaryen of King's Landing",
+    shortName: "Targaryen",
+    id: 378,
+    imgUrl: require("@/assets/img/houses/targaryen_logo.png"),
+    color: "#000",
+  },
+];
+
+export default featuredHouses;
