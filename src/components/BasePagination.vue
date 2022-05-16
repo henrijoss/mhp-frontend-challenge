5c48134faaf8939ad866ea940ab8aaa1155d82db
Henri Joss <henri.joss@ise.fraunhofer.de>
2022-05-16 10:51:52 +0200

    changed pagination style

--- src/components/BasePagination.vue
+++ src/components/BasePagination.vue
@@ -1,37 +1,44 @@
 <template>
   <div>
-    <!-- First -->
-    <button
-      :disabled="isFirstPage"
-      @click="$emit('change-page', 1)"
-      class="btn btn-pagination"
-    >
-      <i class="icon icon-light icon-double-arrow-left"></i>
-    </button>
-    <!-- Prev -->
-    <button
-      :disabled="isFirstPage"
-      @click="$emit('change-page', currentPage - 1)"
-      class="btn btn-pagination"
-    >
-      <i class="icon icon-light icon-arrow-left"></i>
-    </button>
-    <!-- Next -->
-    <button
-      :disabled="isLastPage"
-      @click="$emit('change-page', currentPage + 1)"
-      class="btn btn-pagination"
-    >
-      <i class="icon icon-light icon-arrow-right"></i>
-    </button>
-    <!-- Last -->
-    <button
-      :disabled="isLastPage"
-      @click="$emit('change-page', pageAmount)"
-      class="btn btn-pagination"
-    >
-      <i class="icon icon-light icon-double-arrow-right"></i>
-    </button>
+    <div class="pagination">
+      <div class="banner"></div>
+      <div class="pagination-buttons">
+        <!-- First -->
+        <button
+          :disabled="isFirstPage"
+          @click="$emit('change-page', 1)"
+          class="btn btn-pagination"
+        >
+          <i class="icon icon-light icon-double-arrow-left"></i>
+        </button>
+        <!-- Prev -->
+        <button
+          :disabled="isFirstPage"
+          @click="$emit('change-page', currentPage - 1)"
+          class="btn btn-pagination"
+        >
+          <i class="icon icon-light icon-arrow-left"></i>
+        </button>
+        <!-- Next -->
+        <button
+          :disabled="isLastPage"
+          @click="$emit('change-page', currentPage + 1)"
+          class="btn btn-pagination"
+        >
+          <i class="icon icon-light icon-arrow-right"></i>
+        </button>
+        <!-- Last -->
+        <button
+          :disabled="isLastPage"
+          @click="$emit('change-page', pageAmount)"
+          class="btn btn-pagination"
+        >
+          <i class="icon icon-light icon-double-arrow-right"></i>
+        </button>
+      </div>
+
+      <div class="banner"></div>
+    </div>
   </div>
 </template>
 
