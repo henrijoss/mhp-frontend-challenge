990eb99d7602ade2a896d1b2d5ba3229f575144d
Henri Joss <henri.joss@ise.fraunhofer.de>
2022-05-16 10:53:53 +0200

    moved house details to static file

---
+++ src/static/houseDetails.js
@@ -0,0 +1,39 @@
+const houseDetails = [
+  {
+    name: "Region",
+    icon: "explore",
+    prop: "region",
+  },
+  {
+    name: "Founded",
+    icon: "flag",
+    prop: "founded",
+  },
+  {
+    name: "Died out",
+    icon: "cross",
+    prop: "diedOut",
+  },
+  {
+    name: "Coat of Arms",
+    icon: "coat-of-arms",
+    prop: "coatOfArms",
+  },
+  {
+    name: "Ancestral weapons",
+    icon: "weapons",
+    prop: "ancestralWeapons",
+  },
+  {
+    name: "Seats",
+    icon: "map",
+    prop: "seats",
+  },
+  {
+    name: "Titles",
+    icon: "titles",
+    prop: "titles",
+  },
+];
+
+export default houseDetails;
