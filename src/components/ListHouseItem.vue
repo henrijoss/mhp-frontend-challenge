15fbff505bec37d705b07c687c008d3a29d39ce5
Henri Joss <henri.joss@ise.fraunhofer.de>
2022-05-16 10:52:59 +0200

    moved logic to component

--- src/components/ListHouseItem.vue
+++ src/components/ListHouseItem.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="houses-list">
+  <div v-if="loading" class="houses-list">
+    <SkeletonLoader v-for="index in 9" :key="index" :css-class="'house'" />
+  </div>
+  <div v-else class="houses-list">
+    <div v-if="error" class="error">{{ error }}</div>
     <div v-for="house in houses" :key="house.url" class="house">
       <HouseItem :house="house" />
     </div>
@@ -8,16 +12,26 @@
 
 <script>
 import HouseItem from "@/components/HouseItem.vue";
+import SkeletonLoader from "@/components/SkeletonLoader.vue";
 
 export default {
   name: "ListHouseItem",
   components: {
     HouseItem,
+    SkeletonLoader,
   },
   props: {
     houses: {
       type: Array,
       default: () => [],
+    },
+    error: {
+      type: String,
+      default: null,
+    },
+    loading: {
+      type: Boolean,
+      default: false,
     },
   },
 };
