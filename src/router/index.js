67b70f969eb5c568bd1721e95766524ee2c0686d
Henri Joss <henri.joss@ise.fraunhofer.de>
2022-05-16 10:53:26 +0200

    changed route casing

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,7 @@ const routes = [
   },
   {
     path: "/houses/:id",
-    name: "HouseDetail",
+    name: "house-detail",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
