52f5cdfe229d8a5c701b006bb9dd66cbbacb3731
Henri Joss <henri.joss@ise.fraunhofer.de>
2022-05-17 10:49:16 +0200

    added house detail component

---
+++ src/components/HouseDetail.vue
@@ -0,0 +1,32 @@
+<template>
+  <div v-if="house[detail.prop]">
+    <i class="icon icon-light" :class="'icon-' + detail.icon"></i>
+    <h2>
+      <strong>{{ detail.name }}</strong>
+    </h2>
+    <div v-if="Array.isArray(house[detail.prop])">
+      <p v-for="(detailItem, index) in house[detail.prop]" :key="index">
+        {{ detailItem }}
+      </p>
+    </div>
+    <div v-else>
+      <p>{{ house[detail.prop] }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HouseDetail",
+  props: {
+    house: {
+      type: Object,
+      default: () => {},
+    },
+    detail: {
+      type: Object,
+      default: () => {},
+    },
+  },
+};
+</script>
