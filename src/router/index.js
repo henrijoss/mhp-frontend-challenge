99230c36ae29a751a082a0f73055235f59246749
Henri Joss <henri.joss@ise.fraunhofer.de>
2022-05-17 10:49:25 +0200

    refactor naming

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,7 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "houses" */ "@/views/HouseDetail.vue"),
+      import(/* webpackChunkName: "houses" */ "@/views/HouseDetailView.vue"),
   },
 ];
 
