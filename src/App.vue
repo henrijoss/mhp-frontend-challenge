7bce38292b6ac1f466f3aa14edeb274b7de32666
Henri Joss <henri.joss@ise.fraunhofer.de>
2022-05-20 15:03:56 +0200

    added effects

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,19 @@
 <template>
+  <div class="overlay-effects">
+    <div v-for="index in 200" :key="index" class="snow"></div>
+    <div id="foglayer_01" class="fog">
+      <div class="image01"></div>
+      <div class="image02"></div>
+    </div>
+    <div id="foglayer_02" class="fog">
+      <div class="image01"></div>
+      <div class="image02"></div>
+    </div>
+    <div id="foglayer_03" class="fog">
+      <div class="image01"></div>
+      <div class="image02"></div>
+    </div>
+  </div>
   <TheHeader v-if="!['house-detail'].includes($route.name)" />
   <main><router-view /></main>
   <TheFooter />
